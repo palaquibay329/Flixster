636a12ae40330602eae4fdc2fa90eea03f3fee32
palaquibay329 <palaquibay329@gmail.com>
2022-03-07 01:48:59 -0500

    pt2

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var rvMovies: RecyclerView
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
